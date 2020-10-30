9e5417b0182eef91cce661143abe80748909fdbd
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-10-30 17:50:15 +0300

    UserViewHolder

--- app/src/main/java/by/training/guthubusersearch/adapter/UserViewHolder.kt
+++ app/src/main/java/by/training/guthubusersearch/adapter/UserViewHolder.kt
@@ -2,5 +2,25 @@ package by.training.guthubusersearch.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import by.training.guthubusersearch.R
+import by.training.guthubusersearch.data.User
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.user_item.view.country
+import kotlinx.android.synthetic.main.user_item.view.profileImage
+import kotlinx.android.synthetic.main.user_item.view.userLogin
 
-class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private val userImage = itemView.profileImage
+    private val userLogin = itemView.userLogin
+    private val userCountry = itemView.country
+
+    fun bind(user: User) {
+        Glide.with(itemView.context)
+            .load(user.avatar_url)
+            .centerCrop()
+            .placeholder(R.drawable.ic_baseline_face_24)
+            .into(userImage)
+        userLogin.text = user.login
+        userCountry.text = "Belarus"
+    }
+}
