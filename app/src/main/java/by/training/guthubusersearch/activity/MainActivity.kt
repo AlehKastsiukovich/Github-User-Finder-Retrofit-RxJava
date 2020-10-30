9d977572d67f92e851cd8cdc2d4644a3304cad8d
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-10-30 14:08:17 +0300

    add User class

--- app/src/main/java/by/training/guthubusersearch/activity/MainActivity.kt
+++ app/src/main/java/by/training/guthubusersearch/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package by.training.guthubusersearch
+package by.training.guthubusersearch.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import by.training.guthubusersearch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
