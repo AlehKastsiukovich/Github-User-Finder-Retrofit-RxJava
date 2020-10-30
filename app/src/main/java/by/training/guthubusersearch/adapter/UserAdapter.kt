96024715bb0d81bd08e24db5a4a0295ff5168ced
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-10-30 17:55:07 +0300

    add UserAdapter class

---
+++ app/src/main/java/by/training/guthubusersearch/adapter/UserAdapter.kt
@@ -0,0 +1,26 @@
+package by.training.guthubusersearch.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import by.training.guthubusersearch.R
+import by.training.guthubusersearch.data.User
+
+class UserAdapter : RecyclerView.Adapter<UserViewHolder>() {
+
+    private var userList = mutableListOf<User>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        return UserViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.user_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bind(userList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+}
